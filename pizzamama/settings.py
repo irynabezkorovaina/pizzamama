2f5c450eedccbf5688081318db6111136bc04cdc
admin <admin@admins-MacBook-Pro.local>
2024-01-11 00:21:09 +0200

    updates

--- pizzamama/settings.py
+++ pizzamama/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
-#from pathlib import Path
 import os
+from pathlib import Path
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,7 +79,7 @@ WSGI_APPLICATION = 'pizzamama.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
